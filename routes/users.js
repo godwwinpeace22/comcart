a15ec497c2429d0ebf5685f71fb0981a9a318b62
godwin gabriel <godwinpeace22@gmail.com>
2018-05-20 21:18:10 +0100

    stripe

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ var session = require('client-sessions');
 var User = require('../models/user');
 var Product = require('../models/product');
 var Cart = require('../models/cart');
-
+const stripe = require("stripe")(process.env.secretKey);
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -260,4 +260,20 @@ router.get('/pay', requireLogin, function(req, res, next){
     });
     
   });
+
+router.post('/pay', requireLogin, function(req,res,next){
+  let amount = 500;
+  stripe.customers.create({
+    email: req.body.stripeEmail,
+   source: req.body.stripeToken
+ })
+ .then(customer =>
+   stripe.charges.create({
+     amount,
+     description: "Sample Charge",
+        currency: "usd",
+        customer: customer.id
+   }))
+ .then(charge => res.redirect('/users/pay'));
+})
 module.exports = router;
